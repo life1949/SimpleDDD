ef15b64752d563d3ed8829f4a4441477b2f22756
go1949 <1578997988@qq.com>
2015-01-07 22:31:10 +0800

    Fix CORS First

---
+++ SimpleDDD.Events/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDDD.Events
+{
+    public class Event:IEvent
+    {
+        public Guid ID { get; private set; }
+        public Guid AggregateID { get; set; }
+
+    }
+}
---
+++ SimpleDDD.Events/IEventPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDDD.Events
+{
+    public interface IEventPublisher
+    {
+        void Publisher<T>(T ievent) where T : IEvent;
+    }
+}
