58226bb1cca8080aea87bdf67aef3eb9e9078db8
go1949 <1578997988@qq.com>
2015-01-09 17:11:52 +0800

    周报工作系统建立

---
+++ SimpleDDD.Sample/Controllers/WeekReportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleDDD.Sample.Controllers
+{
+    public class WeekReportController : Controller
+    {
+        //
+        // GET: /WeekReport/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
